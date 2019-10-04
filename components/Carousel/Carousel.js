2ebb28f6101d566ae0d573fea6a6f8f0f162104b
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-10-04 11:53:58 -0600

    attempted stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,60 @@
     <div class="right-button"> > </div>
   </div>
 */
+const carouselContainer = document.querySelector(".carousel-container");
+function Carousel() {
+  const carousel = document.createElement("div"),
+    leftBtn = document.createElement("div"),
+    img1 = document.createElement("img"),
+    img2 = document.createElement("img"),
+    img3 = document.createElement("img"),
+    img4 = document.createElement("img"),
+    rightBtn = document.createElement("div");
+
+  carousel.classList.add("carousel");
+  leftBtn.classList.add("left-button");
+  rightBtn.classList.add("right-button");
+
+  leftBtn.textContent = "<";
+  rightBtn.textContent = ">";
+
+  const carouselWidth = carousel.offsetWidth;
+  const img1Style = img1.currentStyle;
+  const img1MarginRight = Number(img1Style.amrginRight.match(/\d+/g)[0]);
+
+  const img2Style = img2.currentStyle;
+  const img2MarginRight = Number(img2Style.amrginRight.match(/\d+/g)[0]);
+
+  const img3Style = img3.currentStyle;
+  const img3MarginRight = Number(img3Style.amrginRight.match(/\d+/g)[0]);
+
+  const img4Style = img4.currentStyle;
+  const img4MarginRight = Number(img4Style.amrginRight.match(/\d+/g)[0]);
+
+  const imgCount = 4;
+  let offSet = 0;
+  const maxX = -((imgCount / 4) * carouselWidth+
+                (img1MarginRight * (imgCount / 4))-
+                carouselWidth - img1MarginRight);
+
+  leftBtn.addEventListener("click", () => {
+    if (offset !== 0) {
+      offset += carouselWidth + cardMarginRight;
+      carousel.style.transform = `translatex(${offset}px)`;
+    }
+  });
+  rightBtn.addEventListener("click", () => {
+    if (offset !== maxX) {
+      offset -= carouselWidth + img1MarginRight;
+      carousel.style.transform = `translatex(${offset}px)`;
+    }
+  });
+
+  img1.src = "./assets/carousel/mountains.jpeg";
+  img2.src = "/assets/carousel/computer.jpeg";
+  img3.src = "./assets/carousel/trees.jpeg";
+  img4.src = "./assets/carousel/turntable.jpeg";
+
+  return carousel;
+}
+carouselContainer.appendChild(Carousel());
